f30026aa29732f9e381cde36224ed5ec4d100448
laisvidoto <lais.vidoto@gmail.com>
2018-01-30 21:07:56 -0200

    Atualizando App

--- app/src/main/java/com/laisvidoto/desafioconcrete/activity/Home_activity.java
+++ app/src/main/java/com/laisvidoto/desafioconcrete/activity/Home_activity.java
@@ -1,15 +1,143 @@
 package com.laisvidoto.desafioconcrete.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.laisvidoto.desafioconcrete.R;
+import com.laisvidoto.desafioconcrete.adapter.RepoAdapter;
+import com.laisvidoto.desafioconcrete.interfaces.Services;
+import com.laisvidoto.desafioconcrete.model.Repositorio;
+import com.laisvidoto.desafioconcrete.model.RespostaRepositorio;
+import com.laisvidoto.desafioconcrete.util.Api;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class Home_activity extends AppCompatActivity {
+public class Home_activity extends AppCompatActivity
+{
+    private RepoAdapter adapter;
+    private RecyclerView listaRepositorio;
+    private Services gitServices;
+    private ProgressBar progressBar;
+    private boolean isLoading = false;
+    private int currentPage   = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        gitServices      = Api.getGitService();
+        progressBar      = (ProgressBar) findViewById(R.id.progressBar);
+        listaRepositorio = (RecyclerView) findViewById(R.id.listaRepositorios);
+
+        /* Configuração do Adapter */
+        adapter = new RepoAdapter(this, new ArrayList<Repositorio>(0), new RepoAdapter.RepositoryListener()
+        {
+            @Override
+            public void onRepositoryClick(String authorName, String repoName)
+            {
+
+                Intent intent =  new Intent(getApplicationContext(), Lista_activity.class);
+                intent.putExtra("authorName", authorName);
+                intent.putExtra("repoName", repoName);
+                startActivity(intent);
+            }
+        });
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaRepositorio.setLayoutManager(layoutManager);
+        listaRepositorio.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        listaRepositorio.addItemDecoration(itemDecoration);
+
+        if (savedInstanceState != null)
+        {
+            List<Repositorio> repositories = (List<Repositorio>) savedInstanceState.getSerializable("repositories");
+            adapter.updateRepositories(repositories);
+            this.currentPage = savedInstanceState.getInt("currentPage");
+        }
+
+        listaRepositorio.addOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+            {
+                super.onScrolled(recyclerView, dx, dy);
+
+                isLoading = true;
+                int pastVisibleItems, visibleItemsCount, totalItemCount;
+
+                if (dy > 0)
+                {
+                    visibleItemsCount = layoutManager.getChildCount();
+                    pastVisibleItems  = layoutManager.findFirstVisibleItemPosition();
+                    totalItemCount    = layoutManager.getItemCount();
+
+                    if (isLoading)
+                    {
+                        if ((visibleItemsCount + pastVisibleItems) >= totalItemCount)
+                        {
+                            isLoading = false;
+                            progressBar.setVisibility(View.VISIBLE);
+                            loadRepositories(++currentPage);
+                        }
+                    }
+                }
+            }
+        });
+
+        listaRepositorio.setAdapter(adapter);
+    }
+
+    public void loadRepositories(int page)
+    {
+        gitServices.getRepositories(page).enqueue(new Callback<RespostaRepositorio>()
+        {
+            @Override
+            public void onResponse(Call<RespostaRepositorio> call, Response<RespostaRepositorio> response)
+            {
+                if (response.isSuccessful())
+                {
+                    progressBar.setVisibility(View.GONE);
+                    adapter.addAll(response.body().getRepositories());
+                    Log.d("Home_activity", "Repositorios carregados.");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RespostaRepositorio> call, Throwable t)
+            {
+                Toast.makeText(getApplicationContext(), "Carregamento inadequado", Toast.LENGTH_SHORT).show();
+                t.printStackTrace();
+                Log.e("Home_activity", "Erro no API!");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        loadRepositories(currentPage);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("repositories", (Serializable) adapter.getRepositories());
+        outState.putInt("currentPage", currentPage);
     }
 }
--- app/src/main/java/com/laisvidoto/desafioconcrete/activity/Lista_activity.java
+++ app/src/main/java/com/laisvidoto/desafioconcrete/activity/Lista_activity.java
@@ -1,15 +1,131 @@
 package com.laisvidoto.desafioconcrete.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.laisvidoto.desafioconcrete.R;
+import com.laisvidoto.desafioconcrete.adapter.PullAdapter;
+import com.laisvidoto.desafioconcrete.interfaces.Services;
+import com.laisvidoto.desafioconcrete.model.PullRequest;
+import com.laisvidoto.desafioconcrete.util.Api;
 
-public class Lista_activity extends AppCompatActivity {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Lista_activity extends AppCompatActivity
+{
+    private String nomeAutor;
+    private String nomeRepositorio;
+    private String data;
+    private RecyclerView listPullRequest;
+    private PullAdapter adapter;
+    private Services gitService;
+    private List<PullRequest> listaRequests;
+    private ProgressBar progressBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        nomeAutor = getIntent().getStringExtra("authorName");
+        nomeRepositorio  = getIntent().getStringExtra("repoName");
+
+        if (nomeAutor.isEmpty() || nomeRepositorio.isEmpty())
+        {
+            Toast.makeText(this, "carregamento inadequado! ", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        getSupportActionBar().setTitle(nomeRepositorio);
+
+        progressBar     = (ProgressBar) findViewById(R.id.pullProgressBar);
+        gitService   = Api.getGitService();
+        listPullRequest = (RecyclerView) findViewById(R.id.pullList);
+        adapter = new PullAdapter(this, new ArrayList<PullRequest>(0), new PullAdapter.PullRequestListener()
+        {
+            @Override
+            public void onPullRequestClick(String url)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        listPullRequest.setLayoutManager(layoutManager);
+        listPullRequest.setAdapter(adapter);
+        listPullRequest.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        listPullRequest.addItemDecoration(itemDecoration);
+
+        if (savedInstanceState != null)
+        {
+            progressBar.setVisibility(View.GONE);
+            listaRequests = (List<PullRequest>) savedInstanceState.getSerializable("list");
+            adapter.updatePullRequest(listaRequests);
+        } else
+        {
+            loadPullRequests();
+        }
+    }
+
+    public void loadPullRequests()
+    {
+        gitService.getPullRequests(nomeAutor, nomeRepositorio).enqueue(new Callback<List<PullRequest>>()
+        {
+            @Override
+            public void onResponse(Call<List<PullRequest>> call, Response<List<PullRequest>> response)
+            {
+                if ( response.isSuccessful() )
+                {
+                    progressBar.setVisibility(View.GONE);
+                    listaRequests = response.body();
+                    adapter.updatePullRequest(listaRequests);
+                    Log.i("Lista_activity", "Pull requests carregados.");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<PullRequest>> call, Throwable t)
+            {
+                Toast.makeText(getApplicationContext(), "Carregamento inadequado!", Toast.LENGTH_SHORT).show();
+                t.printStackTrace();
+                Log.i("Lista_activity", "Erro no API!");
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("list", (Serializable) listaRequests);
     }
 }
