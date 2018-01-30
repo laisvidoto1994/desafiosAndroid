032dff3a50365e7244f91f3f9ecb219b2079175e
laisvidoto <lais.vidoto@gmail.com>
2018-01-30 15:58:06 -0200

    Atualizando App

---
+++ app/src/main/java/com/laisvidoto/desafioconcrete/Lista.java
@@ -0,0 +1,13 @@
+package com.laisvidoto.desafioconcrete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
